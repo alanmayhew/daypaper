fdd0543678afb202cad1aaa3689ea51f04441e2c
Alan Mayhew <alanlmayhew@gmail.com>
2017-08-20 14:32:05 -0400

    Fixed timezone handling and logic

--- daypaper.py
+++ daypaper.py
@@ -7,10 +7,10 @@ import time
 
 NUM_DAY_TRANSITIONS = 5
 NUM_NIGHT_TRANSITIONS = 3
-FULL_DAY_SECONDS = 24*3600
+FULL_DAY = datetime.timedelta(days=1)
 SLEEP_INTERVAL = 60*5
 
-TIMEZONE = "US/Eastern"
+TIMEZONE = pytz.timezone("US/Eastern")
 IMAGE_DIRECTORY = "./dynamic_wallpaper"
 VALID_EXTENTIONS = [".png", ".jpg", ".jpeg"]
 FEH_COMMAND = "feh --bg-fill {}"
@@ -19,19 +19,6 @@ G_IMAGE_FILES = []
 G_TRANSITION_TIMES = []
 G_TRANSITION_INDEX_TABLE = []
 
-def timeToSeconds(t):
-    return (t.hour*3600 + t.minute*60 + t.second) % FULL_DAY_SECONDS
-
-def secondsToTime(s):
-    s %= FULL_DAY_SECONDS
-    h = 0
-    m = 0
-    h = s/3600
-    s %= 3600
-    m = s/60
-    s %= 60
-    return datetime.time(h,m,s)
-
 def generateLookupTable(l):
     return dict([(j,k) for k,j in enumerate(l)])
 
@@ -39,33 +26,37 @@ def updateTimes(latitude, longitude):
     data = {'lat':latitude, 'lng':longitude}
     resp = requests.get('https://api.sunrise-sunset.org/json', params=data)
     res = resp.json()['results']
-    sunrise = res['sunrise']
-    sunset = res['sunset']
+    sunrise_str = res['sunrise']
+    sunset_str = res['sunset']
     day_length_str = res['day_length']
 
     f = "%I:%M:%S %p"
-    sunrise_time = datetime.datetime.strptime(sunrise, f)
-    sunset_time = datetime.datetime.strptime(sunset, f)
+    sunrise = datetime.datetime.strptime(sunrise_str, f).replace(tzinfo=pytz.utc)
+    sunset = datetime.datetime.strptime(sunset_str, f).replace(tzinfo=pytz.utc)
 
-    day_length = datetime.datetime.strptime(day_length_str, f)
-    day_length = timeToSeconds(day_length)
-    night_length = FULL_DAY_SECONDS - day_length
+    today = datetime.datetime.now().date()
+    sunrise = datetime.datetime.combine(today, sunrise.timetz()).astimezone(TIMEZONE)
+    sunset = datetime.datetime.combine(today, sunset.timetz()).astimezone(TIMEZONE)
 
-    tz = pytz.timezone(TIMEZONE)
-    today = datetime.datetime.today()
-    sunrise_time = datetime.datetime.combine(today, sunrise_time.time())
-    sunrise_time = tz.normalize(sunrise_time.replace(tzinfo=pytz.utc)).time()
-    sunset_time = datetime.datetime.combine(today, sunset_time.time())
-    sunset_time = tz.normalize(sunset_time.replace(tzinfo=pytz.utc)).time()
+    day_length = [int(x) for x in day_length_str.split(':')]
+    day_length = sum([x*(60**i) for i,x in enumerate(day_length[::-1])][::-1])
+    day_length = datetime.timedelta(seconds=day_length)
+    night_length = FULL_DAY - day_length
 
     day_delta = day_length / NUM_DAY_TRANSITIONS
     night_delta = night_length / NUM_NIGHT_TRANSITIONS
 
     transitions = []
     for i in range(NUM_DAY_TRANSITIONS):
-        transitions.append(sunrise_time + day_delta*i)
+        transitions.append(sunrise + (day_delta*i))
     for i in range(NUM_NIGHT_TRANSITIONS):
-        transitions.append(sunset_time + night_delta*i)
+        transitions.append(sunset + (night_delta*i))
+
+    print "SUNRISE:",sunrise
+    print "SUNSET:",sunset
+    print "Transition times:"
+    for i in range(len(transitions)):
+        print "{}: {}".format(i, transitions[i])
 
     return transitions
 
@@ -104,17 +95,16 @@ while True:
         G_IMAGE_FILES = getImageFiles(IMAGE_DIRECTORY)
         G_TRANSITION_TIMES = updateTimes(latitude_in, longitude_in)
         G_TRANSITION_INDEX_TABLE = generateLookupTable(G_TRANSITION_TIMES)
-        current_time = datetime.datetime.now()
+        current_time = datetime.datetime.now(TIMEZONE)
         # set previous time to yesterday at 23:59:59 (before any time today)
-        prev_time = datetime.datetime.combine(current_time, datetime.time()) - datetime.timedelta(seconds=1)
+        prev_time = datetime.datetime.combine(current_time.date(), datetime.time(0,0,0, tzinfo=TIMEZONE))
+        prev_time -= datetime.timedelta(seconds=1)
         print "Updating transition times at",current_time
         print "Found {} files".format(len(G_IMAGE_FILES))
-        tr_str = [str(x) for x in G_TRANSITION_TIMES]
-        print "Transition times: {}".format(', '.join(tr_str))
     else:
         time.sleep(SLEEP_INTERVAL)
         prev_time = current_time
-        current_time = datetime.datetime.now()
+        current_time = datetime.datetime.now(TIMEZONE)
 
     if prev_time > G_TRANSITION_TIMES[-1]:
         update = True
